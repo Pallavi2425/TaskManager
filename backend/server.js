24658d382cd4fab9bdc9cd32ea3cd907ba2f0d28
Pallavi Mishra <pallavimishra102@gmail.com>
2025-02-25 21:51:34 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,9 @@ const port = process.env.PORT || 8000;
 
 const app = express();
 app.use(express.json());
+app.get('/',(req,res)=>{
+ res.status(200).json({message:'Hello from server'});
+})
 
 app.use(express.urlencoded({ extended: false }));
 
